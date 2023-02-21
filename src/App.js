81b87af8cb7c2c517faad7c877d44e524be8df27
Dmytro Hordus <dmytrohordus@Isaacs-MacBook-Pro.local>
2023-02-21 14:45:57 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react"
+import { nanoid } from 'nanoid'
+import Question from "./components/Question"
+
+
+
+
+function App() {
+  const [data, setData] = useState([]);
+  const [newData, setNewData] = useState([]);
+  const [isStarted, setIsStarted] = useState(false);
+  const [showCorrect, setShowCorrect] = useState(false)
+
+  useEffect(() => {
+    const API_URL = `https://opentdb.com/api.php?amount=6`;
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setData(data.results));
+  }, [])
+
+  useEffect(() => {
+    const updatedData = data.map((e) => generateQuizElement(e));
+    setNewData(updatedData);
+  }, [data]);
+
+  function generateQuizElement(e) {
+    const question = e.question.replace(/&quot;|&#039;|&eacute;|&rsquo;|&ocirc;/g, "'");
+    const correctAnswer = e.correct_answer;
+    const incorrectAnswers = e.incorrect_answers;
+    const allAnswers = [correctAnswer, ...incorrectAnswers].sort(
+      () => Math.random() - 0.5
+    );
+
+    return {
+      id: nanoid(),
+      question: question,
+      correctAnswer: correctAnswer,
+      allAnswers: allAnswers,
+      answered: false,
+    };
+  }
+
+  function holdAnswer(answered, id) {
+    setNewData((prev) =>
+      prev.map((question) => {
+        return question.id === id
+          ? { ...question, answered: answered }
+          : question;
+      })
+    );
+  }
+
+  function countHoldAnswer() {
+    let count = 0;
+    newData.forEach((e) => {
+      if (e.answered) {
+        count++;
+      }
+    });
+    return count;
+  }
+
+  function showCorrectAnswer() {
+    if (showCorrect) {
+      setIsStarted(false)
+      setShowCorrect(false)
+    } else {
+      setShowCorrect(true)
+    }
+  }
+
+  const questionElements = newData.map((e) => (
+    <Question
+      key={e.id}
+      question={e.question}
+      correctAnswer={e.correctAnswer}
+      allAnswers={e.allAnswers}
+      holdAnswer={(answered) => holdAnswer(answered, e.id)}
+      showAnswer={showCorrect}
+    />
+  ));
+
+  return (
+    <div className="main">
+      <div className="wrapper">
+        {!isStarted ?
+          <div className="start_menu">
+            <h1 className="title title__1">Quizzical</h1>
+            <h3 className="title title__3">Some description if needed</h3>
+            <button className="button button--control" onClick={() => setIsStarted(true)}>Start game</button>
+          </div>
+          :
+          <>
+            {questionElements}
+            <div className="control-set">
+              {showCorrect && <h2 className="title title__2">You scored {countHoldAnswer()}/{newData.length} correct answers</h2>}
+              <button className="button button--control" onClick={() => showCorrectAnswer()}>{showCorrect ? "Start again" : "Show Answers"}</button>
+            </div>
+          </>
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Question.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "../style.scss"
+
+export default function Question(props) {
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState()
+  const [answerIndex, setAnswerIndex] = useState()
+
+  
+  function handleCLick(e, index) {
+    if(!props.showAnswer){
+      props.holdAnswer(e === props.correctAnswer ? true : false)
+      setIsCorrectAnswer(e === props.correctAnswer ? true : false)
+      setAnswerIndex(index)    
+    }
+  }
+
+  if (props.showAnswer) {
+    console.log("true")
+  } else {
+    console.log("fasle")
+  }
+
+  function style(index) {
+    let color
+    let opacity
+    if (props.showAnswer) {
+      color = answerIndex === index
+        ? isCorrectAnswer
+          ? "#94D7A2"
+          : "#F8BCBC"
+        : ""
+        opacity = answerIndex === index
+        ? isCorrectAnswer
+          ? "1"
+          : ".5"
+        : ".5"
+    } else {
+      color = answerIndex === index
+        ? "#D6DBF5"
+        : ""
+    }
+    return {
+      backgroundColor: color,
+      opacity: opacity,
+    }
+  }
+
+  const questionItem = props.allAnswers.map((e, index) => (
+
+    <button className="button"
+      key={e}
+      onClick={() => handleCLick(e, index)}
+      style={style(index)}
+    >{e.replace(/&quot;|&#039;|&eacute;|&ouml;|&ocirc;/g, "'")}</button>
+  ))
+
+  return (
+    <div className="question">
+      <h2 className="title title__2">{props.question}</h2>
+      <div className="button_set">
+        {questionItem}
+      </div>
+    </div>
+  );
+}
